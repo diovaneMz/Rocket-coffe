71556f9e3512c0451543293a6ffb82935e8654e0
diovaneMz <diovanemarcelinoazevedo@gmail.com>
2022-05-27 17:56:47 -0300

    finalizado

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+function resize() {
+  window.addEventListener("resize", navigationRealocationInHeader);
+}
+
 function openMenu() {
   header.classList.add("menu-expanded");
   navigation.classList.add("menu-expanded");
@@ -16,3 +20,24 @@ function getElemenetByClass(className) {
   const element = document.querySelector(className);
   return element;
 }
+
+function navigationRealocationInHeader() {
+  const header = document.querySelector("header .wrapper");
+  const navigation = document.getElementById("navigation");
+  const wrapper = document.querySelector("#navigation > div");
+  const button = document.querySelector("header .styled-button");
+
+  if (window.innerWidth >= 1024) {
+    wrapper.classList.remove("wrapper");
+
+    header.insertBefore(navigation, button);
+    console.log("bao");
+  } else {
+    wrapper.classList.add("wrapper");
+    body.insertBefore(navigation, main);
+    console.log("opa");
+  }
+}
+
+navigationRealocationInHeader();
+resize();
